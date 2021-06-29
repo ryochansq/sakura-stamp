708bb291dec4e08f84b736311f37e4379779802a
ryochansq <nicestsnsq@gmail.com>
2021-06-29 23:36:42 +0900

    fix files

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,10 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -14,11 +18,45 @@ module.exports = {
     },
     ecmaVersion: 12,
     sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
   },
   plugins: [
     'react',
     '@typescript-eslint',
   ],
   rules: {
-  },
+    'import/extensions': [
+        'error',
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        }
+      ],
+      'react/jsx-filename-extension': [
+        'error',
+        {
+          extensions: ['.jsx', '.tsx'],
+        }
+      ],
+      'no-void': [
+        'error',
+        {
+          allowAsStatement: true,
+        }
+      ],
+      'react/react-in-jsx-scope': 'off',
+      "no-use-before-define": "off",
+      "@typescript-eslint/no-use-before-define": ["error"]
+	},
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  }
 };
